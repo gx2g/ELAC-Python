4204cf99cb464b9b61bb1d76c3349b1737c1a7b7
Robert R <grafx2g@gmail.com>
2024-10-21 15:06:33 -0700

    pj2 project

---
+++ pj2.py
@@ -0,0 +1,25 @@
+# Author: Robert Resendez
+# Date: 10/21/2024
+# Purpose: 
+
+# MAIN PROGRAM
+
+# global varaibles
+n = 1
+
+# ===========================
+
+print("Welcome to the Investment Report Tool by Robert Resendez!")
+print(n, "==========================="); n + 1;
+
+
+# ENTER CODE from pages 75-76
+
+print(n, "==========================="); n + 1;
+print("Thank you for using the Investment Report Tool by Robert Resendez!")
+print(n, "==========================="); n + 1;
+
+x = input("Press Ctrl+Alt+PrtScn to get a snapshot of this console, then Enter to exit: ")
+
+
+# End of MAIN PROGRAM
