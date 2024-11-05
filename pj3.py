b4bf12c1d1489b35b5677ca185feb585650fab23
Robert R <grafx2g@gmail.com>
2024-11-05 09:45:32 -0800

    roll the dice game

---
+++ pj3.py
@@ -0,0 +1,14 @@
+# Author: Robert Resendez
+# Date: 11-05-2024
+# Purpose: JP4 - Game of roll the dice. Earn/Lose money with initial money of 100 dollars. 
+ 
+# Main Program
+n = 1 
+yourMoney = 100
+
+print("---------------" + " " + chr(0x2680) + " " + chr(0x2681) + " " + chr(0x2682) + " " + chr(0x2683) + " " + chr(0x2684) + " " + chr(0x2685) + "----------------");
+
+
+print("welcome to Rolling Dice Game by Robert Resendez" + chr(0x2757))
+print("================================================"); n+=1;
+print("Now, you have $" + str(yourMoney) + " dollars to play with.")
