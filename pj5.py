a1d8c7f8f2d02845f600e30dd373a3659053b7a3
Robert R <grafx2g@gmail.com>
2024-11-08 15:55:22 -0800

    pj5 password game updates

--- pj5.py
+++ pj5.py
@@ -21,15 +21,30 @@ while (pw != "q"):
     countupper = 0
     countyears = 0
 
+
+    # Counting 26 upper case leters:
+    countupper += pw.count('A') + pw.count('B') + pw.count('C') + pw.count('D') + pw.count('E') + pw.count('F') + pw.count('G')
+    countupper += pw.count('H') + pw.count('I') + pw.count('J') + pw.count('K') + pw.count('L') + pw.count('M')
+    countupper += pw.count('N') + pw.count('O') + pw.count('P') + pw.count('Q') + pw.count('R') + pw.count('S')
+    countupper += pw.count('T') + pw.count('U') + pw.count('V') + pw.count('W') + pw.count('X') + pw.count('Y') + pw.count('Z')
+
+
     # Counting 26 lower case leters:
-    countlower += pw.count('a') + pw.count('b') + pw.count('c') + pw.count('e') + pw.count('f') + pw.count('g')
+    countlower += pw.count('a') + pw.count('b') + pw.count('c') + pw.count('d') + pw.count('e') + pw.count('f') + pw.count('g')
     countlower += pw.count('h') + pw.count('i') + pw.count('j') + pw.count('k') + pw.count('l') + pw.count('m')
     countlower += pw.count('n') + pw.count('o') + pw.count('p') + pw.count('q') + pw.count('r') + pw.count('s')
     countlower += pw.count('t') + pw.count('u') + pw.count('v') + pw.count('w') + pw.count('x') + pw.count('y') + pw.count('z')
 
-    if(countupper < 2):
-        print("R1: Your password is not secure for the matrix since it has less than 2 upper-case letters")
+    
+    if(countupper <= 1):
+        print("R1: Your password is not secure since it has less than 2 upper-case letters")
         badcount += 1
+    
+    if(countlower <= 1):
+        print("R2: Your password is not secure since it has less than 2 lower-case letters")
+        badcount += 1
+    
+
 
 
     if badcount == 0:
